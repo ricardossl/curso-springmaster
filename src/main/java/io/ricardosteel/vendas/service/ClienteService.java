06145a181ae8a58dec6ebe927b6ced816964b28c
ricardo-ssl <ricardo.saul@gmail.com>
2022-02-16 16:52:39 -0300

    Teste

--- src/main/java/io/ricardosteel/vendas/service/ClienteService.java
+++ src/main/java/io/ricardosteel/vendas/service/ClienteService.java
@@ -6,4 +6,10 @@ import io.ricardosteel.vendas.domain.entity.Cliente;
 
 public interface ClienteService {
 	Optional<Cliente> getClienteById(Integer id);
+	
+	void saveCliente(Cliente cliente);
+	
+	void deleteClient(Integer id);
+	
+	Cliente updateCliente(Cliente clienteAtualizado, Integer id);
 }
