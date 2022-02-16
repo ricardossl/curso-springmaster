0244eaa0aa2a8d13d2970031500d3d5f9eec51d2
ricardo-ssl <ricardo.saul@gmail.com>
2022-02-16 16:51:40 -0300

    Teste

--- src/main/java/io/ricardosteel/vendas/rest/controller/ClienteController.java
+++ src/main/java/io/ricardosteel/vendas/rest/controller/ClienteController.java
@@ -2,13 +2,19 @@ package io.ricardosteel.vendas.rest.controller;
 
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.ricardosteel.vendas.domain.entity.Cliente;
+import io.ricardosteel.vendas.exceptions.RegraNegocioException;
 import io.ricardosteel.vendas.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 
@@ -16,16 +22,45 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/clientes")
 @RequiredArgsConstructor
 public class ClienteController {
-	private final ClienteService clienteServiceImpl;
+	private final ClienteService service;
 
 	@GetMapping("/{id}")
 	public ResponseEntity<Cliente> getClientById(@PathVariable Integer id) {
-		Optional<Cliente> cliente = clienteServiceImpl.getClienteById(id);
+		Optional<Cliente> cliente = service.getClienteById(id);
 
 		if (cliente.isPresent())
 			return ResponseEntity.ok(cliente.get());
 
-		return ResponseEntity.notFound().build();
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 
+	@PostMapping
+	public ResponseEntity<String> saveCliente(@RequestBody Cliente cliente) {
+		try {
+			service.saveCliente(cliente);
+			return ResponseEntity.status(HttpStatus.CREATED).build();
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(e.getMessage());
+		}
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteClient(@PathVariable Integer id) {
+		try {
+			service.deleteClient(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<String> update(@RequestBody Cliente clienteAtualizado, @PathVariable Integer id) {
+		try {
+			service.updateCliente(clienteAtualizado, id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
 }
--- src/main/java/io/ricardosteel/vendas/serviceimpl/ClienteServiceImpl.java
+++ src/main/java/io/ricardosteel/vendas/serviceimpl/ClienteServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import io.ricardosteel.vendas.domain.entity.Cliente;
 import io.ricardosteel.vendas.domain.repository.ClienteRepository;
+import io.ricardosteel.vendas.exceptions.RegraNegocioException;
 import io.ricardosteel.vendas.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 
@@ -17,6 +18,33 @@ public class ClienteServiceImpl implements ClienteService {
 	@Override
 	public Optional<Cliente> getClienteById(Integer id) {
 		return repository.findById(id);
+	}
+
+	@Override
+	public void saveCliente(Cliente cliente) {
+		repository.save(cliente);
+	}
+
+	@Override
+	public void deleteClient(Integer id) {
+		Optional<Cliente> cliente = getClienteById(id);
+		if (!cliente.isPresent()) {
+			throw new RegraNegocioException("Cliente não encontrado para o id informado");
+		}
+
+		repository.delete(cliente.get());
+	}
+
+	@Override
+	public Cliente updateCliente(Cliente clienteAtualizado, Integer id) {
+		Optional<Cliente> cliente = getClienteById(id);
+		if (!cliente.isPresent()) {
+			throw new RegraNegocioException("Cliente não encontrado para o id informado");
+		}
+
+		clienteAtualizado.setId(cliente.get().getId());
+
+		return repository.save(clienteAtualizado);
 	}
 
 }
